ee6e812c29f90f824db3c91a6e2d5ef24406b9ab
YOUR NAME <longcheng0229@gmail.com>
2015-11-13 17:25:24 -0600

    Nov13,17:30

--- js/longcheng.js
+++ js/longcheng.js
@@ -7,12 +7,12 @@ $(document).ready(function(){
 		$("#rightPart").off("click")
 		if(($("#rightPart").css("left")).replace(/[^-\d\.]/g, '')==$( window ).width()/2){
 			$(this).css("z-index",15)
-			$(this).animate({left:'0'},2000)
+			$(this).animate({left:'0'},1500)
 		}else{
-			$("#rightPart").animate({left:'50%'},1000)
+			$("#rightPart").animate({left:'50%'},800)
 			$(this).css("z-index",15)
 			$("#rightPart").css("z-index",10)
-			$(this).animate({left:'0'},2000)
+			$(this).animate({left:'0'},1500)
 		}
 		setTimeout(function(){
 			$("#leftPart").on("click",leftPartClick)
@@ -25,12 +25,12 @@ $(document).ready(function(){
 		$("#rightPart").off("click")
 		if(($("#leftPart").css("left")).replace(/[^-\d\.]/g, '')<0){
 				$(this).css("z-index",15)
-				$(this).animate({left:'10%'},2000)
+				$(this).animate({left:'10%'},1500)
 			}else{
-				$("#leftPart").animate({left:'-40%'},1000)
+				$("#leftPart").animate({left:'-40%'},800)
 				$(this).css("z-index",15)
 				$("#leftPart").css("z-index",10)
-				$(this).animate({left:'10%'},2000)
+				$(this).animate({left:'10%'},1500)
 		}
 		setTimeout(function(){
 			$("#leftPart").on("click",leftPartClick)
@@ -41,6 +41,24 @@ $(document).ready(function(){
 	$("#leftPart").on("click",leftPartClick)
 	$("#rightPart").on("click",rightPartClick)
 	
+	//left part animation
+	$("#education").click(function(){
+		$(this).addClass("educationMove");
+		$("#education span:first-child").addClass("eduBefore");
+		$(this).siblings().addClass("sibilngsMove");
+	})
 	
+	$("#career").click(function(){
+		$(this).addClass("careerMove");
+		$("#career span:first-child").addClass("carBefore");
+		$("#career span:nth-child(2)").addClass("careerAfterMove");
+		$(this).siblings().addClass("sibilngsMove");
+	})
+	
+	$("#design").click(function(){
+		$(this).addClass("designMove");
+		$("#design span:nth-child(2)").addClass("dsgAfter");
+		$(this).siblings().addClass("sibilngsMove");
+	})
 	
 })
