befa4e5195f1e3d2bd01c5ccae95344ae3df72c4
longcheng24 <longcheng0229@gmail.com>
2015-11-15 00:14:54 -0600

    Nov5,12:14

--- js/longcheng.js
+++ js/longcheng.js
@@ -46,6 +46,10 @@ $(document).ready(function(){
 		$(this).addClass("educationMove");
 		$("#education span:first-child").addClass("eduBefore");
 		$(this).siblings().addClass("sibilngsMove");
+		setTimeout(function(){
+			var leftPosition = ($(window).width()*0.9/2) - $("#education span:first-child").width() - ($("#education span:nth-child(2)").width())/2
+			$("#education").css("left",leftPosition)
+		},2800)
 	})
 	
 	$("#career").click(function(){
@@ -53,12 +57,44 @@ $(document).ready(function(){
 		$("#career span:first-child").addClass("carBefore");
 		$("#career span:nth-child(2)").addClass("careerAfterMove");
 		$(this).siblings().addClass("sibilngsMove");
+		setTimeout(function(){
+			var leftPosition = ($(window).width()*0.9/2) - $("#career span:first-child").width() - ($("#career span:nth-child(2)").width())/2
+			$("#career").css("left",leftPosition)
+		},2800)
 	})
 	
 	$("#design").click(function(){
 		$(this).addClass("designMove");
 		$("#design span:nth-child(2)").addClass("dsgAfter");
 		$(this).siblings().addClass("sibilngsMove");
+		setTimeout(function(){
+			var leftPosition = ($(window).width()*0.9 - $("#design span:first-child").width())/2
+			$("#design").css("left",leftPosition)
+		},2800)
+	})
+	
+	$("#contact").click(function(){
+		$(this).addClass("contactMove");
+		$("#contact span:first-child").addClass("ctcBefore");
+		$("#contact span:nth-child(2)").addClass("contactAfterMove");
+		$(this).siblings().addClass("sibilngsMove");
+		setTimeout(function(){
+			var leftPosition = ($(window).width()*0.9/2) - $("#contact span:first-child").width() - ($("#contact span:nth-child(2)").width())/2
+			$("#contact").css("left",leftPosition)
+		},2800)
+	})
+	
+	$("#about").click(function(){
+		$(this).addClass("aboutMove");
+		$("#about span:nth-child(2)").addClass("aboutAfterMove");
+		$(this).siblings().addClass("sibilngsMove");
+		setTimeout(function(){
+			$("#about span:first-child").text("about");
+		},2000)
+	setTimeout(function(){
+			var leftPosition = ($(window).width()*0.9 - $("#about span:first-child").width())/2
+			$("#about").css("left",leftPosition)
+		},2800)
 	})
 	
 })
